3d502e92d951191294df50a3ace01213dbe62e1a
DavidTopo12 <davidsalgado849@gmail.com>
2022-06-22 21:37:53 -0600

    Estructura Base

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+/*require('dotenv.config')*/ 
+
+const app = express();
+
+app.set('port',3001);
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.listen(app.get('port'), () =>{
+    console.log("Servidor iniciado en el puerto " + app.get('port'));
+});
+
