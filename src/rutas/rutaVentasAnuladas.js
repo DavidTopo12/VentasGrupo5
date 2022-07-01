eb0bdde41baf7b84e42ffdba9123f04782c0416d
DavidTopo12 <davidsalgado849@gmail.com>
2022-06-30 20:34:43 -0600

    cambios; correccion errores

--- src/rutas/rutaVentasAnuladas.js
+++ src/rutas/rutaVentasAnuladas.js
@@ -2,17 +2,17 @@ const { Router } = require('express');
 const { body, query } = require('express-validator');
 const controladorVentasAnuladas = require('../controladores/controladorVentasAnuladas');
 const rutas = Router();
+
+rutas.get('/listar', controladorVentasAnuladas.Listar);
+
+
 rutas.post('/agregar',
-body('usua').
-notEmpty().withMessage('No aceptan campos vacios')
+body('usua')
+.notEmpty().withMessage('No aceptan campos vacios')
 .isInt().withMessage('Solo aceptan numero Entero'),
-
-body('des').
-notEmpty().withMessage('No aceptan campos vacios')
-.isString({min:250}).withMessage('Solo aceptan numero Entero'),
-
-
-
-controladorVentasAnuladas.Agregar);
+body('des')
+.notEmpty().withMessage('No aceptan campos vacios')
+.isString({min:3}).withMessage('Minimo 3 caracteres'),
+controladorVentasAnuladas.AgregarVentaAnulada);
 
 module.exports = rutas;
