9574a753d77b5a323931260cee8a7f1725359606
DavidTopo12 <davidsalgado849@gmail.com>
2022-07-19 23:43:09 -0600

    GUARDADOS EN CLIENTE Y EMPLEADOS IMAGEN

--- src/controladores/controladorArchivo.js
+++ src/controladores/controladorArchivo.js
@@ -2,6 +2,7 @@ const { Console } = require('console');
 const fs = require('fs');
 const path = require('path');
 const empleados = require('../modelos/modeloEmpleados');
+const clientes = require('../modelos/modeloclientes');
 const MSJ = require('../componentes/mensaje');
 
 
@@ -49,6 +50,49 @@ exports.Recibir = async (req, res) => {
     }
 };
 
+
+exports.RecibirCliente = async (req, res) => {
+    console.log(req);
+    const { filename } = req.file;
+    const { id } = req.body;
+   
+    try{
+    
+        var buscarCliente = await clientes.findOne({
+            where:{
+                idregistro: id
+            }
+        });
+        if(!buscarCliente){
+            const buscarImagen = fs.existsSync(path.join(__dirname, '../public/img/clientes/' + filename));
+            
+            if(buscarImagen){
+                fs.unlinkSync(path.join(__dirname, '../public/img/clientes/' + filename));
+                console.log('Imagen eliminada');
+            }
+            res.send('el id del cliente no existe')
+        }
+        else{
+            const buscarImagen = fs.existsSync(path.join(__dirname, '../public/img/clientes/' + buscarCliente.nombreImagen));
+            if(buscarImagen){
+                fs.unlinkSync(path.join(__dirname, '../public/img/clientes/' + buscarCliente.nombreImagen));
+                console.log('Imagen eliminada');
+            }
+            
+            buscarCliente.nombreImagen=filename;
+            await buscarCliente.save()
+            .then((data)=>{
+                res.json(data);
+            })
+            .catch((error)=>{
+                res.json(error);
+            });
+        }
+        ;
+    }catch(error){
+        res.json(error);
+    }
+};
 
 
 
--- src/rutas/rutaArchivo.js
+++ src/rutas/rutaArchivo.js
@@ -16,9 +16,27 @@ const storageEmpleados = multer.diskStorage({
     }
 });
 
+const storageClientes = multer.diskStorage({
+
+    // definir el lugar donde se almacenará la imagen
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '../public/img/clientes'));
+    },
+
+    // creando un nombre dku para el archivo
+    filename: function(req, file, cb){
+
+        const nombre = Date.now() + "-" + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + "-" + nombre + file.mimetype.replace("/", "."));
+    }
+});
+
+
 const uploadEmpleados = multer({storage: storageEmpleados});
+const uploadClientes =  multer({storage: storageClientes});
 const controladorArchivos = require('../controladores/controladorArchivo');
 const rutas = Router();
 rutas.post('/empleados/img', uploadEmpleados.single('img'), controladorArchivos.Recibir);
+rutas.post('/clientes/img', uploadClientes.single('img'), controladorArchivos.RecibirCliente);
 
 module.exports = rutas;
