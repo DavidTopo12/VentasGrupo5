5e82a8ecbfcbcf25c5ed66ffd7f4c08770413216
DavidTopo12 <davidsalgado849@gmail.com>
2022-07-22 21:11:22 -0600

    Validación Usuarios

--- src/rutas/rutasUsuarios.js
+++ src/rutas/rutasUsuarios.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const { body, query } = require('express-validator');
 const controladorUsuarios = require('../controladores/controladorUsuarios');
+const passport = require('../configuraciones/passport');
 const rutas = Router();
 
 rutas.get('/', controladorUsuarios.Inicio);
 
-rutas.get('/listar', controladorUsuarios.Listar);
+rutas.get('/listar',passport.ValidarAutendicado, controladorUsuarios.Listar);
 
 module.exports = rutas;
